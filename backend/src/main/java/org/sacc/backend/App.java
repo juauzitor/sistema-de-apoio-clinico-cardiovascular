5c3e6a85cde5f3a8ec113ca8f91d4249d3a3d46c
João Vitor Mendes Pinto dos Santos <joao.v.m.p.santos@ieee.org>
2024-06-02 02:21:07 -0300

    feat(backend/*): Configuração inicial do tomcat docker e jakarta

---
+++ backend/src/main/java/org/sacc/backend/App.java
@@ -0,0 +1,18 @@
+package org.sacc.backend;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "App", urlPatterns = {"/"})
+public class App extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().println("Hello, World!");
+    }
+}
